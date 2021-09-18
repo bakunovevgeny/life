d7faca8b8a0517749041b589d8a61894af58228c
bakunovevgeny <79792912+bakunovevgeny@users.noreply.github.com>
2021-09-18 15:54:38 +0700

    lufe

--- life/life/Life.cpp
+++ life/life/Life.cpp
@@ -1,7 +1,8 @@
 #include "life.h"
 #include <cstdlib>
-#include <time.h>
+#include <ctime>
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 Life::Life() {
@@ -16,7 +17,19 @@ Life::~Life()
 
 int Life::checkRule(int i, int j)
 {
-    return 0;
+    int lifeCount = 0;
+    for (int k = i - 1; k < i+2; k++)
+        for (int n = j - 1; n < j+2; n++)
+            lifeCount = lifeCount + space[k][n];
+    lifeCount = lifeCount - space[i][j];
+
+    if (lifeCount == 3 && space[i][j] == 0)
+        space[i][j] = 1;
+
+    if ((lifeCount < 2 || lifeCount > 3) && space[i][j] == 1)
+        space[i][j] = 0;
+
+    return space[i][j];
 }
 
 void Life::printSpace()
@@ -35,11 +48,22 @@ void Life::generateLife()
 {
     for (int i = 0; i < a; i++)
         for (int j = 0; j < b; j++)
-            space[i][j] = rand() % 2;
+            space[i][j] = (rand() + time(0)) % 2;
 }
 
 
 void Life::runLife() {
     generateLife();
     printSpace();
+    Sleep(1000);
+
+    system("cls");
+    while (1) {
+        for (int i = 0; i < a; i++)
+            for (int j = 0; j < b; j++)
+                checkRule(i, j);
+        printSpace();
+        Sleep(50);
+        system("cls");
+    }
 }
