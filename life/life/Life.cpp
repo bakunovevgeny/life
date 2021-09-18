164352a1971eff609654f0721bc9a2a3cfb9476f
bakunovevgeny <79792912+bakunovevgeny@users.noreply.github.com>
2021-09-18 22:15:45 +0700

    заготовка под окно

--- life/life/Life.cpp
+++ life/life/Life.cpp
@@ -8,28 +8,43 @@ using namespace std;
 Life::Life() {
     for (int i = 0; i < a; i++)
         for (int j = 0; j < b; j++)
+        {
             space[i][j] = 0;
+        }
 }
 
 Life::~Life()
 {
 }
 
+int Life::countNaightbours(int x, int y)
+{
+    int count = 0;
+
+    for (int i = -1; i < 2; i++)
+    {
+        for (int j = -1; j < 2; j++)
+        {
+            int col = (x + i + spaceX) % spaceX;
+            int row = (y + j + spaceY) % spaceY;
+            bool isSelfChecking = col == x && row == y;
+            if (!isSelfChecking && space[i][j] == 1)
+                count++;
+        }
+    }
+    return count;
+}
+
 int Life::checkRule(int i, int j)
 {
-    int lifeCount = 0;
-    for (int k = i - 1; k < i+2; k++)
-        for (int n = j - 1; n < j+2; n++)
-            lifeCount = lifeCount + space[k][n];
-    lifeCount = lifeCount - space[i][j];
+    int lifeCount = countNaightbours(i,j);
+ //   int newSpace[a][b];
 
-    if (lifeCount == 3 && space[i][j] == 0)
+    if ((lifeCount == 2 || lifeCount == 3) && space[i][j] == 0)
         space[i][j] = 1;
-
     if ((lifeCount < 2 || lifeCount > 3) && space[i][j] == 1)
         space[i][j] = 0;
-
-    return space[i][j];
+    return 0;
 }
 
 void Life::printSpace()
@@ -37,9 +52,9 @@ void Life::printSpace()
     for (int i = 0; i < a; i++) {
         for (int j = 0; j < b; j++)
             if (space[i][j] == 1)
-                cout << "*";
+                cout << "* ";
             else
-                cout << " ";
+                cout << "  ";
         cout << endl;
     }
 }
@@ -51,9 +66,41 @@ void Life::generateLife()
             space[i][j] = (rand() + time(0)) % 2;
 }
 
+void Life::Planer()
+{
+    for (int i = 0; i < a; i++)
+        for (int j = 0; j < b; j++)
+            space[i][j] = 0;
+    space[5][5] = space[5][6] = space[5][7] = 1;
+
+    //space[0][1] = space[1][2] = space[2][0] = space[2][1] = space[2][2] = 1;
+}
+
+void Life::Queen_bee()
+{
+    for (int i = 0; i < a; i++)
+        for (int j = 0; j < b; j++)
+            space[i][j] = 0;
+
+    space[5][5] = space[5][6] = space[6][7] = space[7][8] = space[8][8] = space[9][8] = space[10][7] = space[11][6] = space[11][5] = 1;
+}
 
 void Life::runLife() {
-    generateLife();
+    int num_g;
+
+    cout << endl << "enter ¹pattern";
+    cout << endl << "1-Random; 2-Planer; 3-Queen bee;";
+    
+    cin >> num_g;
+    
+    switch (num_g)
+    {
+        case 1:generateLife(); break;
+        case 2:Planer(); break;
+        case 3:Queen_bee(); break;
+    }
+    system("cls");
+
     printSpace();
     Sleep(1000);
 
@@ -63,7 +110,7 @@ void Life::runLife() {
             for (int j = 0; j < b; j++)
                 checkRule(i, j);
         printSpace();
-        Sleep(50);
+        Sleep(500);
         system("cls");
     }
 }
--- life/life/Life.h
+++ life/life/Life.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define spaceX 10
-#define spaceY 10
+#define spaceX 20
+#define spaceY 20
 
 const int a = spaceX + 2;
 const int b = spaceY + 2;
@@ -11,9 +11,12 @@ class Life
 
     int space[a][b];
 
+    int countNaightbours(int x, int y);
     int checkRule(int i, int j);
     void printSpace();
     void generateLife();
+    void Planer();
+    void Queen_bee();
 
 public:
     Life();
