a5c98516b5fe27fba38fb3cfe826e75f739dcebc
bakunovevgeny <79792912+bakunovevgeny@users.noreply.github.com>
2021-09-19 13:19:19 +0700

    ui

--- life/Life UI/life UI/Form1.Designer.cs
+++ life/Life UI/life UI/Form1.Designer.cs
@@ -30,12 +30,146 @@ namespace life_UI
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bStop = new System.Windows.Forms.Button();
+            this.bStart = new System.Windows.Forms.Button();
+            this.nudDensity = new System.Windows.Forms.NumericUpDown();
+            this.nudResolution = new System.Windows.Forms.NumericUpDown();
+            this.resolution = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.density = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudResolution)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.density);
+            this.splitContainer1.Panel2.Controls.Add(this.bStop);
+            this.splitContainer1.Panel2.Controls.Add(this.bStart);
+            this.splitContainer1.Panel2.Controls.Add(this.nudDensity);
+            this.splitContainer1.Panel2.Controls.Add(this.nudResolution);
+            this.splitContainer1.Panel2.Controls.Add(this.resolution);
+            this.splitContainer1.Size = new System.Drawing.Size(1202, 553);
+            this.splitContainer1.SplitterDistance = 1049;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1045, 549);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bStop
+            // 
+            this.bStop.Location = new System.Drawing.Point(15, 142);
+            this.bStop.Name = "bStop";
+            this.bStop.Size = new System.Drawing.Size(120, 23);
+            this.bStop.TabIndex = 6;
+            this.bStop.Text = "Стоп";
+            this.bStop.UseVisualStyleBackColor = true;
+            this.bStop.Click += new System.EventHandler(this.bStop_Click);
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(15, 113);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(120, 23);
+            this.bStart.TabIndex = 5;
+            this.bStart.Text = "Старт";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // nudDensity
+            // 
+            this.nudDensity.Location = new System.Drawing.Point(15, 78);
+            this.nudDensity.Name = "nudDensity";
+            this.nudDensity.Size = new System.Drawing.Size(120, 20);
+            this.nudDensity.TabIndex = 7;
+            // 
+            // nudResolution
+            // 
+            this.nudResolution.Location = new System.Drawing.Point(15, 32);
+            this.nudResolution.Name = "nudResolution";
+            this.nudResolution.Size = new System.Drawing.Size(120, 20);
+            this.nudResolution.TabIndex = 9;
+            // 
+            // resolution
+            // 
+            this.resolution.AutoSize = true;
+            this.resolution.Location = new System.Drawing.Point(12, 16);
+            this.resolution.Name = "resolution";
+            this.resolution.Size = new System.Drawing.Size(70, 13);
+            this.resolution.TabIndex = 1;
+            this.resolution.Text = "Разрешение";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // density
+            // 
+            this.density.AutoSize = true;
+            this.density.Location = new System.Drawing.Point(12, 62);
+            this.density.Name = "density";
+            this.density.Size = new System.Drawing.Size(118, 13);
+            this.density.TabIndex = 10;
+            this.density.Text = "Плотность заселения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1202, 553);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudResolution)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown nudResolution;
+        private System.Windows.Forms.Label resolution;
+        private System.Windows.Forms.NumericUpDown nudDensity;
+        private System.Windows.Forms.Button bStop;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label density;
     }
 }
 
--- life/Life UI/life UI/Form1.cs
+++ life/Life UI/life UI/Form1.cs
@@ -12,9 +12,30 @@ namespace life_UI
 {
     public partial class Form1 : Form
     {
+        private Graphics graphics;
+        private int res;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            res = (int)nudResolution.Value;
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            graphics.FillRectangle(Brushes.Crimson, 0, 0, res, res);
+        }
+
+        private void bStop_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
