3c4445603db6f11264d220479340ccea2512eeb4
bakunovevgeny <79792912+bakunovevgeny@users.noreply.github.com>
2021-10-23 01:41:32 +0700

    123

---
+++ Life UI/LifeTest/LifeTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LifeTest
+{
+    [TestClass]
+    public class LifeTests
+    {
+        [TestMethod]
+        public void NeightboursC()
+        {
+
+        }
+    }
+}
---
+++ Life UI/LifeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LifeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LifeTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bc563847-c790-48d9-93d4-3eec75e2600e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
